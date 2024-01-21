c7b05d0add1d3bce809ede28504d2c8f4b64e9bb
sajjad <st.655.st@gmail.com>
2024-01-21 19:31:53 +0330

    Improve structure and styling in components/header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -21,91 +21,89 @@ export default function Header() {
   };
 
   return (
-    <Container maxWidth="xl">
-      <header className="py-4 flex flex-row items-center justify-between">
-        {/* Social media icons section */}
-        <div className="h-fit flex flex-row align-center justify-center min-w-48">
-          <Link href="https://www.instagram.com/atlantisrp.ir/" target="_blank">
-            <IconButton color="primary" aria-label="delete">
-              <Image
-                src="/images/instagram.svg"
-                alt="atlantis-rp-instagram"
-                width={25}
-                height={25}
-                draggable="false"
-              />
-            </IconButton>
-          </Link>
-          <Link href="https://www.reddit.com/r/AtlantisRP" target="_blank">
-            <IconButton color="primary" aria-label="delete">
-              <Image
-                src="/images/reddit.svg"
-                alt="atlantis-rp-instagram"
-                width={25}
-                height={25}
-                draggable="false"
-              />
-            </IconButton>
-          </Link>
-          <Link href="https://twitter.com/_AtlantisRP_" target="_blank">
-            <IconButton color="primary" aria-label="delete">
-              <Image
-                src="/images/x.svg"
-                alt="atlantis-rp-instagram"
-                width={25}
-                height={25}
-                draggable="false"
-              />
-            </IconButton>
-          </Link>
-          <Link href="https://discord.com/invite/iratlantisrp" target="_blank">
-            <IconButton color="primary" aria-label="delete">
-              <Image
-                src="/images/discord.svg"
-                alt="atlantis-rp-discord"
-                width={25}
-                height={25}
-                draggable="false"
-              />
-            </IconButton>
-          </Link>
-        </div>
-
-        {/* Atlantis RP logo section */}
-        <Link href="/">
-          <Image
-            className={styles.headerImg}
-            src="/images/atlantis-rp-logo.png"
-            alt="atlantis rp"
-            height={50}
-            width={50}
-            draggable="false"
-          />
+    <Container component="header" className="py-4 flex flex-row items-center justify-between">
+      {/* Social media icons section */}
+      <div className="h-fit flex flex-row align-center justify-center min-w-48">
+        <Link href="https://www.instagram.com/atlantisrp.ir/" target="_blank">
+          <IconButton color="primary" aria-label="delete">
+            <Image
+              src="/images/instagram.svg"
+              alt="atlantis-rp-instagram"
+              width={25}
+              height={25}
+              draggable="false"
+            />
+          </IconButton>
         </Link>
+        <Link href="https://www.reddit.com/r/AtlantisRP" target="_blank">
+          <IconButton color="primary" aria-label="delete">
+            <Image
+              src="/images/reddit.svg"
+              alt="atlantis-rp-instagram"
+              width={25}
+              height={25}
+              draggable="false"
+            />
+          </IconButton>
+        </Link>
+        <Link href="https://twitter.com/_AtlantisRP_" target="_blank">
+          <IconButton color="primary" aria-label="delete">
+            <Image
+              src="/images/x.svg"
+              alt="atlantis-rp-instagram"
+              width={25}
+              height={25}
+              draggable="false"
+            />
+          </IconButton>
+        </Link>
+        <Link href="https://discord.com/invite/iratlantisrp" target="_blank">
+          <IconButton color="primary" aria-label="delete">
+            <Image
+              src="/images/discord.svg"
+              alt="atlantis-rp-discord"
+              width={25}
+              height={25}
+              draggable="false"
+            />
+          </IconButton>
+        </Link>
+      </div>
 
-        {/* Toggle buttons for page navigation */}
-        <div className="h-fit flex flex-row align-center justify-center min-w-48">
-          <ToggleButtonGroup
-            value={pageStat}
-            exclusive
-            className={styles.styledToggleButtonGroup}
-            onChange={handlePageStat}
-            aria-label="text alignment"
-          >
-            <Link href="/rules">
-              <ToggleButton value="rules" aria-label="left aligned">
-                قوانین
-              </ToggleButton>
-            </Link>
+      {/* Atlantis RP logo section */}
+      <Link href="/">
+        <Image
+          className={styles.headerImg}
+          src="/images/atlantis-rp-logo.png"
+          alt="atlantis rp"
+          height={50}
+          width={50}
+          draggable="false"
+        />
+      </Link>
 
-            <Link href="/">
-              <ToggleButton value="home" aria-label="centered">
-                صفحه اصلی
-              </ToggleButton>
-            </Link>
-          </ToggleButtonGroup>
-        </div>
-      </header>
+      {/* Toggle buttons for page navigation */}
+      <div className="h-fit flex flex-row align-center justify-center min-w-48">
+        <ToggleButtonGroup
+          value={pageStat}
+          exclusive
+          className={styles.styledToggleButtonGroup}
+          onChange={handlePageStat}
+          aria-label="text alignment"
+        >
+          <Link href="/rules">
+            <ToggleButton value="rules" aria-label="left aligned">
+              قوانین
+            </ToggleButton>
+          </Link>
+
+          <Link href="/">
+            <ToggleButton value="home" aria-label="centered">
+              صفحه اصلی
+            </ToggleButton>
+          </Link>
+        </ToggleButtonGroup>
+      </div>
     </Container>
   );
 }
