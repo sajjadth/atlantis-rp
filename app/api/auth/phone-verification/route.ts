a9e60455f3ca041ce7c01378df5ef5ba53c1cd80
sajjad <st.655.st@gmail.com>
2024-03-01 11:44:51 +0330

    Enhance phone verification route
    This commit enhances the phone verification route (pp\api\auth\phone-verification\route.ts) by improving the logic for checking phone number registration and verification code activation. It introduces the isPhoneNumberRegistered function to accurately determine if a phone number is already registered and verified. Additionally, it updates error message formatting for better clarity and readability. These changes improve the reliability and effectiveness of the phone verification process.

--- app/api/auth/phone-verification/route.ts
+++ app/api/auth/phone-verification/route.ts
@@ -10,15 +10,25 @@ function generateVerificationCode(): string {
   return (Math.floor(Math.random() * (MAX_CODE - MIN_CODE + 1)) + MIN_CODE).toString();
 }
 
-// Check if the phone number is registered and verified
-function isPhoneNumberRegistered(): boolean {
-  return cookies().has("phone_number") && Boolean(cookies().get("user_verified")?.value);
+/**
+ * Check if the phone number is registered and verified
+ * @param number - The phone number to check
+ * @returns A boolean indicating whether the phone number is registered and verified
+ */
+function isPhoneNumberRegistered(number: string): boolean {
+  return (
+    cookies().has("phone_number") &&
+    cookies().has("user_verified") &&
+    JSON.parse(cookies().get("user_verified")?.value as string) &&
+    cookies().get("phone_number")?.value === number
+  );
 }
 
 // Check if the verification code is active and not expired
 function isVerificationCodeActive(): boolean {
   return (
-    Boolean(cookies().get("user_verified")?.value) &&
+    cookies().has("user_verified") &&
+    JSON.parse(cookies().get("user_verified")?.value as string) &&
     cookies().has("verification_code_expiration_time") &&
     (cookies().get("verification_code_expiration_time")?.value as string) >
       new Date().getTime().toString()
@@ -28,8 +38,8 @@ function isVerificationCodeActive(): boolean {
 // Set cookies for the phone number, the code, and the expiration time
 function setVerificationCookies(phoneNumber: string, code: string): void {
   const expirationTime = String(new Date().setMinutes(new Date().getMinutes() + 2));
-  cookies().set("verification-code", code);
-  cookies().set("user_verified", isPhoneNumberRegistered() ? "true" : "false");
+  cookies().set("verification_code", code);
+  cookies().set("user_verified", isPhoneNumberRegistered(phoneNumber) ? "true" : "false");
   cookies().set("phone_number", phoneNumber);
   cookies().set("verification_code_expiration_time", expirationTime);
 }
@@ -40,22 +50,22 @@ export async function POST(req: NextRequest) {
   const data = await req.json();
 
   // Return an error message if the phone number is already registered or the code is active
-  if (isPhoneNumberRegistered())
+  if (isPhoneNumberRegistered(data.phoneNumber))
     return NextResponse.json({
       success: false,
-      message: `Oops! It seems that this phone number is already associated with an account. If you believe this is a mistake or need further assistance, please contact support.`,
+      message: `Phone number already in use. Contact support if needed.`,
     });
 
   if (isVerificationCodeActive())
     return NextResponse.json({
       success: false,
-      message: `Sorry, you cannot request a new verification code yet. Please wait for the current code to expire before requesting another one.`,
+      message: `Verification code not ready. Wait for expiration.`,
     });
 
   // Set cookies and return a success message
   setVerificationCookies(data.phoneNumber, code);
   return NextResponse.json({
     success: true,
-    message: `Verification code sent successfully to ${data.phoneNumber}`,
+    message: `Verification code sent successfully`,
   });
 }
